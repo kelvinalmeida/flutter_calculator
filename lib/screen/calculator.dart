87abe14e5484174cf475028bda421c4eda2b24d2
Kelvin Almeida <58817347+kelvinalmeida@users.noreply.github.com>
2023-07-22 11:52:53 -0300

    Initial commit

---
+++ lib/screen/calculator.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_calculator/widget/buttons.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: (size.height - 30) / 2,
+          ),
+          Buttons(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widget/buttons.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_calculator/widget/gridItem.dart';
+
+class Buttons extends StatelessWidget {
+  const Buttons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
+    return Expanded(
+      child: GridView.count(
+        padding: const EdgeInsets.all(10),
+        childAspectRatio: size.width * 4 / size.height,
+        crossAxisCount: 2,
+        children: [
+          Container(
+            color: Colors.cyan,
+            width: 50,
+            height: 50,
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GridItem(content: '1'),
+                GridItem(content: '2'),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              GridItem(content: '3'),
+              GridItem(content: '-'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              GridItem(content: '4'),
+              GridItem(content: '5'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              GridItem(content: '6'),
+              GridItem(content: 'X'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              GridItem(content: '7'),
+              GridItem(content: '8'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              GridItem(content: '9'),
+              GridItem(content: '/'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              GridItem(content: '0'),
+              GridItem(content: '.'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              GridItem(content: '='),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
